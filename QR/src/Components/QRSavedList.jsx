056038a61948b87046961d05015cdfa7d344915a
Mesandu2007 <mesandugunarwardhana@gmail.com>
2026-02-12 12:59:21 +0530

    Added the final search functionality

--- QR/src/Components/QRSavedList.jsx
+++ QR/src/Components/QRSavedList.jsx
@@ -1,22 +1,45 @@
-export default function QRSavedList({savedQrs,onSelect}){
-  if(savedQrs.length===0){
-    return <p style={{textAlign: "center"}}>No saved QR codes!</p>;
-  }
-  
-  return(
-    <div className="saved-list">
-      {savedQrs.map((item) => (
-        <div
-          key={item.id}
-          className="qr-card"
-          onClick={() => onSelect(item)}
-        >
-          <QRCodeCanvas value={item.link} size={120} />
-          <p>{item.name}</p>
-          <p>{item.link}</p>
+import {useState} from "react";
+import {QRCodeCanvas} from "qrcode.react"
 
-        </div>
-      ))}
+export default function QRSavedList({savedQrs,onSelect}){
+  const[searchTerm,setSearchTerm]=useState("");
+
+  if(savedQrs.length===0){
+    return<p style={{textAlign:"center"}}>No saved QR codes!</p>;
+  }
+  const filteredQrs=savedQrs.filter((qr)=>
+    qr.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="saved-list-container">
+      
+      <input
+        type="text"
+        placeholder="Search by QR Name..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="qr-search"
+      />
+
+      <div className="saved-list">
+        {filteredQrs.length === 0 && (
+          <p style={{ textAlign: "center" }}>No QR codes match your search!</p>
+        )}
+        {filteredQrs.map((item) => (
+          <div
+            key={item.id}
+            className="qr-card"
+            onClick={() => onSelect(item)}
+          >
+            <QRCodeCanvas value={item.link} size={120} />
+            <p>{item.name}</p>
+            <p>{item.link}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
+
+
 }
